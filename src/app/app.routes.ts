99b631d794c01e816da03ccb4669c922da9dbf51
Daniel MC <mitmacontreras@gmail.com>
2025-10-09 00:11:11 -0500

    Dashboard and Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './Shared/components/layout/layout.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+   { path: '', redirectTo: 'login', pathMatch: 'full' },
+   { path: 'login', loadComponent: () => 
+        import('./authentication/login/login.component').then(m => m.LoginComponent) },
+   { path: 'layout', loadComponent: () =>
+        import('./Shared/components/layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+        {
+            path: 'dashboard', loadComponent: () =>
+                import('./business/dashboard/dashboard.component').then(m => m.DashboardComponent)
+        },
+        {path:'', redirectTo:'dashboard', pathMatch:'full'}
+    ]},
+   { path: '**', redirectTo: 'login' }
+
+
+];
