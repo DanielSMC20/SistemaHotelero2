4810bcc9744d03069db3d33fc58b6a7a89b9a5c9
AlessPe <alesandrocaveroalvarez474@gmail.com>
2025-10-13 00:19:24 -0500

    Components is already oñoo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./Shared/components/sidebar/sidebar.component";
+import { HeaderComponent } from "./Shared/components/header/header.component";
+import { FooterComponent } from "./Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Sistema Hotelero';
+  title = 'SistemaHotelero';
 }
